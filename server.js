4cedc20e90f836a1c0d2160d9a6ea02ee2516679
Bonnie Tsao <IceJade1788@gmail.com>
2019-01-03 17:51:11 -0800

    removed old css comments

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require(`express`);
+const express = require(`express`)
 const app = express()
 
-app.use(express.static('public')) //'if you see a static file, try taking a look at the directory "public", and see if it's there'; since we have a static about page in public folder: localhost:8080 since we have an index.html in the public folder
+app.use(express.static('public'))
 
 app.listen(8080, function() {
     console.log('Listening on Port 8080')
