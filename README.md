63eae202e5c83775c8309b74d71f02d13e99081a
Bonnie Tsao <IceJade1788@gmail.com>
2019-02-14 18:01:04 -0800

    Added a Procfile

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Fan Website
-This project is to showcase my HTML, CSS, Bootstrap and Sass skills. 
+This project is to showcase my HTML, CSS, Bootstrap skills.
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require(`express`)
-const app = express()
+const express = require(`express`);
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.listen(8080, function() {
-    console.log('Listening on Port 8080')
-})
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
